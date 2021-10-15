0747750cf115443ee009ccdcac3999fc953609a9
Gaurav <gs97197@gmail.com>
2021-10-15 16:34:42 +0530

    Comment added

--- force-app/main/default/lwc/editTabLevelAccessCmp/editTabLevelAccessCmp.js
+++ force-app/main/default/lwc/editTabLevelAccessCmp/editTabLevelAccessCmp.js
@@ -1,15 +1,31 @@
 import { api, LightningElement, track } from 'lwc';
 
 export default class EditTabLevelAccessCmp extends LightningElement {
+    /**
+     * Tab selected in the parent component
+     * @type {String}
+     */
     @api
     tabName;
+    /**
+     * Existin tab access for the tab in profile
+     * @type {String}
+     */
     @api
     tabAccess;
+    /**
+     * List for generating access combobox
+     * @type {List}
+     */
     tabAccessChoice = [
         { value: "Hidden", label: "Hidden" },
         { value: "DefaultOn", label: "Default On" },
         { value: "DefaultOff", label: "Default Off" }
     ];
+    /**
+     * This method will fire event when we change tab level access
+     * @param {Event} evt 
+     */
     relayDataToParent(evt) {
         this.dispatchEvent(new CustomEvent('tabaccesschange', { detail: { tabName: this.tabName, tabAccess: evt.currentTarget.value } }));
     }
